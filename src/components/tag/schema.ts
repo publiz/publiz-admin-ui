2345178e3c059126f907b03bcd8d0bd46044e88e
tranviettrung <viettrung2580@gmail.com>
2024-02-05 16:26:25 +0700

    create tags

--- src/components/tag/schema.ts
+++ src/components/tag/schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const createTagSchema = z.object({
   name: z.string().min(1).max(100),
   slug: z.string().min(1).max(100),
-  type: z.string(),
+  type: z.enum(["SYSTEM", "DEFAULT"]),
   organizationId: z.number(),
   userId: z.number(),
 });
